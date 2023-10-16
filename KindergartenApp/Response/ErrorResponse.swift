1a23ad4ac7b259a677f2463f7b0f64a150e49c2e
kdh5018 <rlaehgns745@gmail.com>
2023-10-16 16:53:21 +0900

    코드 리팩토링(ViewController 길었던 코드 ViewModel로 옮김)

--- KindergartenApp/Response/ErrorResponse.swift
+++ KindergartenApp/Response/ErrorResponse.swift
@@ -10,3 +10,15 @@ import Foundation
 struct ErrorResponse: Codable {
     let message: String?
 }
+
+enum MenuError: Error {
+    case invalidDefaultSelection(String)
+}
+
+func validateMenuDefaultSelection(_ menu: [String]) throws {
+    // 만약 메뉴가 비어 있다면, 에러를 던집니다.
+    if menu.isEmpty {
+        throw MenuError.invalidDefaultSelection("Menu does not have a valid element for default selection")
+    }
+    
+}
